6d27b5823167c8c622600ae5ed94f94d75ce5090
Jacob Diehl <jakediehl17@gmail.com>
2026-02-02 08:41:33 -0700

    update naming for image

--- components/courses/individual_courses/Opening.jsx
+++ components/courses/individual_courses/Opening.jsx
@@ -14,7 +14,7 @@ export default function OneDayCourses() {
                     <div className={styles.photos}>
                         <figure className={`${styles.polaroid} ${styles.p1}`}>
                             <img
-                                src="public/courses/One Day Course nov7.png"
+                                src="public/courses/One_Day_Course_nov7.png"
                                 alt="Students practicing skills outdoors"
                                 className={styles.photo}
                             />
